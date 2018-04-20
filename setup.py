b14e2e9c2e28cad71e2fc5e3996cf3c9d18085ce
Jens Schaa <jens.schaa@posteo.de>
2018-04-20 13:38:08 +0200

    remove classifiers from setup.py

--- setup.py
+++ setup.py
@@ -29,16 +29,5 @@ setup(
     license='Apache Software License',
     install_requires=dependencies,
     test_suite='test.test_data',
-    long_description=open('README.md').read(),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
-    ]
+    long_description=open('README.md').read()
 )
